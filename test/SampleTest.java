68d308bf1b8a83f3169324dea11b680011500cae
Temi Taylor <temit@mit.edu>
2023-06-19 15:45:35 -0400

    Setting up project

---
+++ test/SampleTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class SampleTest {
+    
+    @Test
+    public void test() {
+        assertEquals(2, 1+1);
+    }
+}
