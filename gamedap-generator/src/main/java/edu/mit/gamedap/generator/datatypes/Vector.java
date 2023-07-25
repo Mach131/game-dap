5878679537028c1a92ca322fd637ca1ab5526ae9
Temi Taylor <rocktalonttaylor@gmail.com>
2023-07-25 16:40:53 -0400

    Tests for string vector

--- gamedap-generator/src/main/java/edu/mit/gamedap/generator/datatypes/Vector.java
+++ gamedap-generator/src/main/java/edu/mit/gamedap/generator/datatypes/Vector.java
@@ -8,23 +8,28 @@ public interface Vector<T> {
 
   /**
    * Randomly re-initializes the values in the vector.
+   * 
+   * @see Vector#randomElement()
    */
   public void randomize();
 
   /**
    * Provides a random element of the vector type.
+   * 
    * @return a random value of the type T
    */
   public T randomElement();
 
   /**
    * Returns the current size of the vector.
+   * 
    * @return the number of elements the vector contains.
    */
   public int size();
 
   /**
    * Gets an element in the vector.
+   * 
    * @param i index
    * @return the value at index i
    */
@@ -32,6 +37,7 @@ public interface Vector<T> {
 
   /**
    * Sets an element in the vector.
+   * 
    * @param i index
    * @param v new value
    * @return the value previously at index i
@@ -39,7 +45,9 @@ public interface Vector<T> {
   public T set(int i, T v);
 
   /**
-   * Calculates the distance between two vectors by soome metric.
+   * Calculates the distance between two vectors by some metric. Expects that
+   * the two vectors have the same size.
+   * 
    * @param other the "destination" vector
    * @return the distance between this vector and the other
    */
---
+++ gamedap-generator/src/test/java/edu/mit/gamedap/generator/datatypes/StringVectorTest.java
@@ -0,0 +1,107 @@
+package edu.mit.gamedap.generator.datatypes;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Test;
+
+public class StringVectorTest {
+  
+  ////// get, set, size
+
+  private void checkStringVector(StringVector vector, String string) {
+    assertEquals("Unexpected vector size", vector.size(), string.length());
+    for (int i = 0; i < string.length(); i++) {
+      assertEquals("Unexpected value in vector", string.charAt(i), (char) vector.get(i));
+    }
+  }
+
+  @Test
+  public void testStringVector_Empty() {
+    StringVector vector = new StringVector("");
+    checkStringVector(vector, "");
+  }
+
+  @Test
+  public void testStringVector_Singleton() {
+    StringVector vector = new StringVector("R");
+    checkStringVector(vector, "R");
+
+    char previous = vector.set(0, 'S');
+    assertEquals("Unexpected return value from set", 'R', previous);
+    checkStringVector(vector, "S");
+  }
+
+  @Test
+  public void testStringVector_Normal() {
+    StringVector vector = new StringVector("hello");
+    checkStringVector(vector, "hello");
+
+    char previous = vector.set(3, 'b');
+    assertEquals("Unexpected return value from set", 'l', previous);
+    checkStringVector(vector, "helbo");
+  }
+
+
+  ////// randomElement
+
+  @Test
+  public void testRandomElement_SingletonSet() {
+    Set<Character> characterSet = new HashSet<>(Arrays.asList('a'));
+    StringVector vector = new StringVector("aaa", characterSet);
+    char random = vector.randomElement();
+    assertEquals("Unexpected random element returned", 'a', random);
+  }
+
+  @Test
+  public void testRandomElement_LargerSet() {
+    Set<Character> characterSet = new HashSet<>(Arrays.asList('a', 'b', 'c', 'd', 'e', 'f'));
+    StringVector vector = new StringVector("aaa", characterSet);
+
+    for (int i = 0; i < 5; i ++) {
+      char random = vector.randomElement();
+      assertTrue("Unexpected random element returned", characterSet.contains(random));
+    }
+  }
+
+  ////// randomize - omitted due to mostly just being randomElement
+
+  ////// distance
+
+  @Test
+  public void testDistance_Identical() {
+    String string = "hello";
+    StringVector v1 = new StringVector(string);
+    StringVector v2 = new StringVector(string);
+    assertEquals("Unexpected distance", 0, v1.distance(v2), 0.01);
+    assertEquals("Distances not symmetric", v2.distance(v1), v1.distance(v2), 0.01);
+  }
+
+  @Test
+  public void testDistance_Single() {
+    StringVector v1 = new StringVector("shoe");
+    StringVector v2 = new StringVector("show");
+    assertEquals("Unexpected distance", 1, v1.distance(v2), 0.01);
+    assertEquals("Distances not symmetric", v2.distance(v1), v1.distance(v2), 0.01);
+  }
+
+  @Test
+  public void testDistance_Multiple() {
+    StringVector v1 = new StringVector("hello world");
+    StringVector v2 = new StringVector("fellow cold");
+    assertEquals("Unexpected distance", 5, v1.distance(v2), 0.01);
+    assertEquals("Distances not symmetric", v2.distance(v1), v1.distance(v2), 0.01);
+  }
+
+  @Test
+  public void testDistance_Disjoint() {
+    StringVector v1 = new StringVector("aeiou");
+    StringVector v2 = new StringVector("qwert");
+    assertEquals("Unexpected distance", 5, v1.distance(v2), 0.01);
+    assertEquals("Distances not symmetric", v2.distance(v1), v1.distance(v2), 0.01);
+  }
+}
